8d468be180e08335625d430bb4dde1bf3a400e44
沈震 <shenzhen@chenzhendeMacBook-Air.local>
2017-09-19 16:33:21 +0800

    submit

---
+++ easyFun/easyFun/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  easyFun
+//
+//  Created by 沈震 on 2017/9/19.
+//  Copyright © 2017年 shenzhen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ easyFun/easyFun/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  easyFun
+//
+//  Created by 沈震 on 2017/9/19.
+//  Copyright © 2017年 shenzhen. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
